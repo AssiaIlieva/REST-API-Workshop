34f9cd82b559b592217dbeba1b3a6584fb3e3abb
AssiaIlieva <ac17@abv.bg>
2024-06-26 18:03:09 +0300

    Add routes and userController

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello world'
-    })
-})
+app.use(cors({
+    
+    origin:'http://localhost:3000'
+}));
+
+app.use(express.json());
 
 
-app.listen(5000, () => console.log('server is listening on port 5000...'));
+
+app.get('/data/catalog', (req, res) => {
+    res.json([])
+});
+
+app.use(routes);
+
+
+app.listen(3030, () => console.log('server is listening on port 3030...'));
