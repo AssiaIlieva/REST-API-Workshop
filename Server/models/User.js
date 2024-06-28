f87aa0045072dc65a2fdb85eb33d1d71e04e8cba
AssiaIlieva <ac17@abv.bg>
2024-06-28 11:00:45 +0300

    Update User model to hash the password before saving

--- Server/models/User.js
+++ Server/models/User.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const userSchema = new mongooseShema({
+const userSchema = new mongoose.Schema({
     email: String,
     password: String,
 });
+
+userSchema.pre('save', async function () {
+    this.password = await bcrypt.hash(this.password, 12);
+})
 
 const User = mongoose.model('User', userSchema);
 
