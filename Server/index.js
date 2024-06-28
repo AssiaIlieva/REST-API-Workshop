bba48dacbfabf923a962e843893cd8e14a4dd53f
AssiaIlieva <ac17@abv.bg>
2024-06-28 10:59:59 +0300

    Connect with the DB

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
@@ -18,6 +19,9 @@ app.get('/data/catalog', (req, res) => {
 });
 
 app.use(routes);
+
+mongoose.connect('mongodb://127.0.0.1:27017/furniture')
+    .then(() => console.log('DB Connected'));
 
 
 app.listen(3030, () => console.log('server is listening on port 3030...'));
