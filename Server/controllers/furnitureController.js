67b316b34a0f48cb9380a1988aec522f76f82c1d
AssiaIlieva <ac17@abv.bg>
2024-06-29 20:04:13 +0300

    Update the furniture functionalities in furnitureController and funitureService

--- Server/controllers/furnitureController.js
+++ Server/controllers/furnitureController.js
@@ -1,17 +1,38 @@
 const router = require('express').Router();
+const qs = require('querystring')
 const furnitureService = require('../services/funitureService');
 
 router.get('/', async(req, res) => {
-    const furnitures = await furnitureService.getAll();
+    const query = qs.parse(req.query.where);
+    const ownerId = query._ownerId?.replace(/"/g, '');
+    const furnitures = await furnitureService.getAll(ownerId);
     res.json(furnitures)
-})
+});
 
-router.post('/', async(req, res) => {
+router.post('/', async(req, res, next) => {
     const furnitureData = req.body;
-    const furniture = await furnitureService.create(furnitureData);
-    res.json(furniture);
-})
+    try {
+        const furniture = await furnitureService.create({...furnitureData, owner: req.user._id});
+        res.json(furniture); 
+    } catch (err) {
+        next(err);
+    }
+});
 
+router.get('/:furnitureId', async(req, res) => {
+    const furniture = await furnitureService.getOne(req.params.furnitureId);
+    res.json(furniture)
+});
 
+router.put('/:furnitureId', async(req, res) => {
+    const furnitureData = req.body
+    const furniture = await furnitureService.edit(req.params.furnitureId, furnitureData);
+    res.json(furniture)
+});
+
+router.delete('/:furnitureId', async(req, res) => {
+    await furnitureService.delete(req.params.furnitureId);
+    res.json({ok: true})
+});
 
 module.exports = router;
--- Server/services/funitureService.js
+++ Server/services/funitureService.js
@@ -1,5 +1,17 @@
 const Furniture = require('../Models/Furniture')
 
-exports.getAll = () => Furniture.find();
+exports.getAll = (ownerId) => {
+    let query = Furniture.find();
+    if(ownerId){
+        query = Furniture.find({_ownerId: ownerId})
+    }
+    return Furniture.find(query)
+ } 
 
 exports.create = (furnitureData) => Furniture.create(furnitureData);
+
+exports.getOne = (furnitureId) => Furniture.findById(furnitureId);
+
+exports.edit = (furnitureId, furnitureData) => Furniture.findByIdAndUpdate(furnitureId, furnitureData);
+
+exports.delete = (furnitureId) => Furniture.findByIdAndDelete(furnitureId);
