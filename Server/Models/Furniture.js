547f2530ce86ab987c41f4a69b738264b440fec6
AssiaIlieva <ac17@abv.bg>
2024-06-29 19:59:16 +0300

    Update the Furniture model

--- Server/Models/Furniture.js
+++ Server/Models/Furniture.js
@@ -1,13 +1,21 @@
 const mongoose = require('mongoose');
 
 const furnitureSchema = new mongoose.Schema({
-    make: String,
+    make: {
+    type: String,
+    required: true,
+    minLength: 8,
+    },
     model: String,
     year: Number,
     description: String,
     price: Number,
     img: String,
     material: String,
+    _ownerId: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }
 });
 
 
